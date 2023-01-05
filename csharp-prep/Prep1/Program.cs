2398235330f935107ca22cdc8b2086c6caa6332a
bottojuli <ino22002@byui.edu>
2023-01-05 20:23:55 -0300

    Finished Program 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");  
+        // store the input in a variable
+        string name = Console.ReadLine(); 
+
+        Console.Write("What is your last name? ");  
+        // store the input in a variable
+        string lastname = Console.ReadLine(); 
+
+        Console.WriteLine($"Your name is {lastname}, {name} {lastname}.");
     }
 }
