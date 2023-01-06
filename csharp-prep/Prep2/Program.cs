73cf5f9276420813b294bef5afa3d19a0fe948d6
bottojuli <ino22002@byui.edu>
2023-01-05 21:29:32 -0300

    Finished Project 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? "); 
+        string userInput = Console.ReadLine();   
+          
+        // converts the string input variable into an int so we can compare numbers in the if blocks
+        int grade = int.Parse(userInput); 
+ 
+        // declare a letter variable for storing the grades inside of the blocks
+        string letter = "";
+
+        if (grade >= 90)  
+        { 
+            letter = "A";
+        }
+        else if (grade >= 80) 
+        { 
+            letter = "B";
+        }
+        else if (grade >= 70) 
+        { 
+            letter = "C";
+        }
+        else if (grade >= 60) 
+        { 
+            letter = "D";
+        }
+        else if (grade < 60) 
+        { 
+            letter = "F";
+        }  
+         
+        // prints the grade
+        Console.WriteLine($"Your grade is {letter}"); 
+ 
+
+        // tells the student is ThreadPriority passed or not
+         if (grade >= 70)  
+        { 
+           Console.WriteLine("Congratulations! You passed the class!"); 
+        } 
+        else 
+        { 
+            Console.WriteLine("Sorry, you didn't pass the class, but keep striving for next semester!");
+        }
+
     }
 }
